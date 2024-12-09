2cadade9eec067359ee51bba3b55bc283939a4ce
Aqib Ilyas <aqibilyas289@gmail.com>
2024-12-09 23:11:13 +0500

    Home Page

--- my-app/src/components/Ceramics.tsx
+++ my-app/src/components/Ceramics.tsx
@@ -1,13 +1,25 @@
- 
- const Ceramics  = () =>{
-    return(
+import Image from "next/image";
+const Ceramics = () => {
+    return (
         <main>
             <div>
-                <p className="clashDisplayFont text-xl">New Ceramic</p>
-                <button></button>
+                <p className="clashdisplayFont text-xl">New Ceramic</p>
+                <div>
+                    <div>
+                        <Image src="/DandyChair.png" alt="Dandy Chair" width={163} height={201} />
+                        <div>
+                            <p>The Dandy Chair</p>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <button className="satoshiFont text-base">View collection</button>
             </div>
         </main>
     )
- }
+}
 
- export default Ceramics;
+export default Ceramics;
