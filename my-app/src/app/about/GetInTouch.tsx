436af09afa287de93cc47043e206a7d5bea9fdcf
Aqib Ilyas <aqibilyas289@gmail.com>
2024-12-22 10:54:00 +0500

    Updated

--- my-app/src/app/about/GetInTouch.tsx
+++ my-app/src/app/about/GetInTouch.tsx
@@ -2,19 +2,22 @@ const GetInTouch = () => {
     return (
         <>
             <main>
-                <div>
-                    <img src="/Images/GetInTouch.png" alt="Get In Touch" />
-                </div>
-
-                <div className="p-5 ">
-                    <div>
-                        <p className="clashdisplayFont text-xl">Our service isn&apos;t just personal, it&apos;s actually hyper personally exquisite</p>
-                        <p className="satoshiFont text-sm mt-6">When we started Avion, the idea was simple. Make high quality furniture affordable and available for the mass market.</p>
-                        <p className="satoshiFont text-sm mt-4"> Handmade, and lovingly crafted furniture and homeware is what we live, breathe and design so our Chelsea boutique become the hotbed for the London interior design community.</p>
+                <div className="medium:flex medium:gap-8">
+                    <div className="medium:flex-1">
+                        <img src="/Images/GetInTouch.png" alt="Get In Touch" className="tablet:w-full medium:h-full" />
                     </div>
 
-                    <div>
-                        <button className="w-full satoshiFont text-base p-10">Get in touch</button>
+                    <div className="p-5 medium:flex-1 medium:pr-20">
+                        <div>
+                            <p className="clashdisplayFont text-xl">Our service isn&apos;t just personal, it&apos;s actually hyper personally exquisite</p>
+                            <p className="satoshiFont text-sm mt-6">When we started Avion, the idea was simple. Make high quality furniture affordable and available for the mass market.</p>
+                            <p className="satoshiFont text-sm mt-4"> Handmade, and lovingly crafted furniture and homeware is what we live, breathe and design so our Chelsea boutique become the hotbed for the London interior design community.</p>
+                        </div>
+
+                        <div>
+                            <button className="w-full satoshiFont text-base p-10">Get in touch</button>
+                        </div>
+
                     </div>
 
                 </div>
--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -3,6 +3,9 @@ import Navbar from "@/app/about/Navbar";
 import HeroBlock from "@/app/about/HeroBlock";
 import FeaturesAdditional from "@/app/about/FeaturesAdditional";
 import GetInTouch from "@/app/about/GetInTouch";
+import Features from "@/app/about/Features";
+import EmailSignup from "@/app/about/EmailSignup";
+import Footer from "@/app/about/Footer";
 
 const AboutPage = () => {
     return (
@@ -12,6 +15,9 @@ const AboutPage = () => {
             <HeroBlock />
             <FeaturesAdditional />
             <GetInTouch />
+            <Features />
+            <EmailSignup />
+            <Footer />
         </>
     )
 }
