6cc4ee767702da425c9e359987dfc9f3a90bbf4b
Aqib Ilyas <aqibilyas289@gmail.com>
2025-01-24 10:26:28 +0500

    .

--- my-app/src/sanity/schemaTypes/index.ts
+++ my-app/src/sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import {product} from '@/sanity/schemaTypes/product'
+import {Category} from '@/sanity/schemaTypes/category'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [product, Category],
 }
